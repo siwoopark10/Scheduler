edfe1b402a38c6cbd7db5b545f88d4d2dc222cba
siwoopark10 <57846202+siwoopark10@users.noreply.github.com>
2021-05-06 15:20:42 -0500

    login

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ScheduleScreen from './screens/ScheduleScreen';
 import CourseDetailScreen from './screens/CourseDetailScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import CourseEditScreen from './screens/CourseEditScreen';
 import UserContext from './UserContext';
+import { Button } from 'react-native';
+import { firebase } from './firebase';
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [user, setUser] = useState({ role: 'admin'});
+  const [auth, setAuth] = useState();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((auth) => {
+      setAuth(auth);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (auth && auth.uid) {
+      const db = firebase.database().ref('users').child(auth.uid);
+      const handleData = snap => {
+        setUser({uid: auth.uid, ...snap.val()});
+      }
+      db.on('value', handleData, error => alert(error));
+      return () => { db.off('value', handleData); };
+    } else {
+      setUser(null);
+    }
+  }, [auth]);
+
+  const SignInButton = ({ navigation, user }) => (
+    user && user.uid
+      ? <Button title="Logout" color="#448aff"
+        onPress={() => firebase.auth().signOut()}
+      />
+      : <Button title="SignIn" color="#448aff"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      />
+  );
 
   return (
     <UserContext.Provider value={user}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Schedule Screen"
+          <Stack.Screen name="ScheduleScreen"
             component={ScheduleScreen}
-            options={{ title: 'Schedule' }}
+            options={({ navigation }) => ({
+              title: "Schedule",
+              headerRight: () => (
+                <SignInButton navigation={navigation} user={user} />
+              ),
+            })
+            }
           />
           <Stack.Screen name="CourseEditScreen"
             component={CourseEditScreen}
@@ -27,6 +67,7 @@ const App = () => {
             component={CourseDetailScreen}
             options={{ title: 'Course Detail' }}
           />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/database";
+import "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAbKQU251jxnu8kwTbvPstMP9aeZGPUPd0",
@@ -12,6 +13,15 @@ const firebaseConfig = {
     measurementId: "G-GLQQXLTDNW"
 };
 
+async function loginWithEmail(email, password) {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+}
+
+async function registerWithEmail(email, password) {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+}
+
 firebase.initializeApp(firebaseConfig);
 
-export { firebase };
+
+export { firebase, loginWithEmail, registerWithEmail };
